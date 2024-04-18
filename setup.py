235494244c9c93e9a5182ab7d1284a3e962f11e0
Yoorhim Cho <yourmejo@gmail.com>
2024-04-19 01:21:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -99,11 +99,11 @@ if not SKIP_CUDA_BUILD:
     cc_flag = []
     if CUDA_HOME is not None:
         _, bare_metal_version = get_cuda_bare_metal_version(CUDA_HOME)
-        if bare_metal_version < Version("11.6"):
-            raise RuntimeError(
-                f"{PACKAGE_NAME} is only supported on CUDA 11.6 and above.  "
-                "Note: make sure nvcc has a supported version by running nvcc -V."
-            )
+        # if bare_metal_version < Version("11.6"):
+            # raise RuntimeError(
+            #     f"{PACKAGE_NAME} is only supported on CUDA 11.6 and above.  "
+            #     "Note: make sure nvcc has a supported version by running nvcc -V."
+            # )
             
     cc_flag.append("-gencode")
     cc_flag.append("arch=compute_53,code=sm_53")
